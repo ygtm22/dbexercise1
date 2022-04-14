610ee988006951f775a03105a98f0939720949e3
yagitomu <22yagi_tomu@axiz.co.jp>
2022-04-14 12:43:49 +0900

    演習問題-SQL1-基本

---
+++ db_exam1.sql
@@ -0,0 +1,18 @@
+select * from major
+
+select * from student
+
+select * from student
+where grade = 1
+
+select * from student
+where hometown = '“Œ‹ž'
+
+select maojor_name from major
+
+update student
+set grade = 3
+where student_id = 10
+
+delete from student
+where student_id = 10
