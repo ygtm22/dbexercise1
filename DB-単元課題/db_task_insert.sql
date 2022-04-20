03f88dc82b4c1b0305c059a8173d674eba066c84
yagitomu <22yagi_tomu@axiz.co.jp>
2022-04-20 17:48:52 +0900

    DB-単元課題-join追加

--- DB-単元課題/db_task_insert.sql
+++ DB-単元課題/db_task_insert.sql
@@ -1,11 +1,8 @@
 insert into item
 (item_id, item_name, price, discount_price, consumptiontax_id)
-values (1, 'いろはす天然水', '100', '-10', 1)
-
-insert into item
-(item_id, item_name, price, discount_price, consumptiontax_id)
-values (2, 'ツナマヨおにぎり', '120', '0', 1)
-, (3, 'ファブリーズ', '400', '0', 2)
+values (1, 'いろはす天然水', 100, -10, 1)
+, (2, 'ツナマヨおにぎり', 120, 0, 1)
+, (3, 'ファブリーズ', '400', 0, 2)
 
 insert into payment
 (payment_id, payment)
@@ -34,20 +31,67 @@ values (1, '大城')
 
 insert into cashregister
 (cashregister_id, cashregister_no)
-values (1, '001')
-, (2, '002')
-, (3, '003')
+values (1, 001)
+
+insert into cashregister
+(cashregister_id, cashregister_no)
+values (2, 002)
+,(3, 003)
 
 insert into receipt
-(receipt_id)
-values (1)
+(receipt_id, receiptdetails_id)
+values (1, 1)
+, (2, 2)
+, (3, 3)
 
-insert into slip
-(silp_id, silp_no)
+insert into silp
+(slip_id, slip_no)
 values (1, 000001)
 , (2, 000002)
 , (3, 000003)
 
+insert into receiptdetails
+(receiptdetails_id, conveniencestore_id, purchasedate, cashregister_id, responsibleperson_id, item_id, payment_id, slip_id)
+values (1, 1, '2022/4/1（金）', 1, 1, 1, 1, 1)
+
+insert into receiptdetails
+(receiptdetails_id, conveniencestore_id, purchasedate, cashregister_id, responsibleperson_id, item_id, payment_id, slip_id)
+values (2, 1, '2022/4/1（金）', 1, 1, 2, 1, 1)
+, (3, 1, '2022/4/1（金）', 1, 1, 3, 1, 1)
+
+
+select
+conveniencestore_name
+, address
+, tel
+, cashregister_no
+, Responsibleperson_name
+, purchasedate
+, item_name
+, price
+, discount_price
+, consumptiontax
+, payment
+, slip_no
+from item i
+join consumptiontax c
+on i.consumptiontax_id = c.consumptiontax_id
+join receiptdetails rt
+on rt.item_id = i.item_id
+join conveniencestore cs
+on rt.conveniencestore_id = cs.conveniencestore_id
+join cashregister cg
+on rt.cashregister_id = cg.cashregister_id
+join payment p
+on rt.payment_id = p.payment_id
+join silp sl
+on rt.slip_id = sl.slip_id
+join responsibleperson rp
+on rt.responsibleperson_id = rp.responsibleperson_id
+group by item_name, price, discount_price, consumptiontax, purchasedate, conveniencestore_name, address, tel, cashregister_no, payment, slip_no, Responsibleperson_name
+order by purchasedate
+
+--確認用
 select * from item;
 
 select * from payment;
